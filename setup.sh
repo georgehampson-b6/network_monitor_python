1dcb8d375e58f845dbdd9d577e55eb33baadb69f
georgehampson-b6 <148126658+georgehampson-b6@users.noreply.github.com>
2024-03-20 12:47:21 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install python3-venv if not already installed
+if ! dpkg -s python3-venv >/dev/null 2>&1; then
+    echo "Installing python3-venv..."
+    sudo apt update
+    sudo apt install python3-venv
+fi
+
+# Define the name of your virtual environment
+VENV_NAME="network_monitoring"
+
+mkdir /home/bolt6/network-monitoring
+
+# Navigate to your project directory
+cd /home/bolt6/network-monitoring
+
+# Check if the virtual environment already exists
+if [ ! -d "$VENV_NAME" ]; then
+    # Create the virtual environment
+    python3 -m venv $VENV_NAME
+fi
+
+# Activate the virtual environment
+source $VENV_NAME/bin/activate
+
+# Install required packages
+pip install -r requirements.txt
+
+echo "Setup complete. Your virtual environment '$VENV_NAME' is ready."
